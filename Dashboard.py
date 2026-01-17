1eddd707fd8a2c2e93dd0fe3a2805f1ea08ea406
Prince Yadav <py63535@gmail.com>
2026-01-17 18:10:04 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 st.markdown('<style>div.block-container{padding-top:1rem;}',unsafe_allow_html=True)
 image = Image.open("adidas-logo.jpg")
 
-col1,col2=st.columns([0.1,0.9], vertical_alignment="center")
+col1,col2=st.columns([0.1,0.9])
 with col1:
     st.image(image,width=100)
 
